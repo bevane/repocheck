c525e475b5f0ad9a5ed62dd22094388799e92a0e
bevane <bevane@live.com>
2024-10-10 11:08:41 +0700

    feat: add tsv flag

--- app/output.go
+++ app/output.go
@@ -31,3 +31,17 @@ func ConstructPlainTable(repos []Repo) string {
 	// return tableBuilder.String()
 	return ""
 }
+
+func ConstructTSVOutput(repos []Repo) string {
+	output := "Name\tPath\tLastModified\tSyncedWithRemote\tSyncDescription\n"
+	for _, repo := range repos {
+		year, month, day := repo.LastModified.Date()
+		lastModifiedDate := fmt.Sprintf("%04d-%02d-%02d", year, int(month), day)
+		oneLineDescription := strings.Join(
+			strings.Split(repo.SyncDescription, "\n"), " ",
+		)
+		row := fmt.Sprintf("%s\t%s\t%s\t%t\t%s\n", repo.Name, repo.AbsPath, lastModifiedDate, repo.SyncedWithRemote, oneLineDescription)
+		output += row
+	}
+	return output
+}
