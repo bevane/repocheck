7cdc29f954a8c418a192f10a1cdb3ebbeec0891f
bevane <bevane@live.com>
2024-12-17 17:30:43 +0700

    style: improve and add more comments

--- app/app.go
+++ app/app.go
@@ -23,14 +23,21 @@ type Repo struct {
 	Author           string    `json:"author"`
 }
 
+// recursively traverses all paths in 'root' and returns a slice of local git Repos
+// that were found along with additional details for each repo
+// the fetch argument determines if a git fetch is ran for each repo before
+// getting rest of the repo details
 func GetReposWithDetails(root string, fetch bool) ([]Repo, error) {
 	var wg sync.WaitGroup
 	fsys := os.DirFS(root)
+	// gather all the repo paths first so that each repo can be concurrently
+	// processed below
 	repoPaths, err := listRepoPaths(fsys)
 	repos := make([]Repo, len(repoPaths))
 	if err != nil {
 		return nil, err
 	}
+	// concurrency is necessary because git fetch is a lengthy blocking call
 	for i, path := range repoPaths {
 		wg.Add(1)
 		go func(i int, path string) {
@@ -44,19 +51,23 @@ func GetReposWithDetails(root string, fetch bool) ([]Repo, error) {
 			if fetch {
 				err = gitFetch(absPath)
 				if err != nil {
+					// continue without returning because git fetch can fail due to
+					// network issues and the rest of the repo details can likely be
+					// gathered
 					slog.Warn(fmt.Sprintf("Unable to run git fetch at %v, %v", absPath, err))
 				}
 			}
 			lastModified, err := getContentLastModifiedTime(dirFS)
-			// dont skip and only log a warning if lastmodified date could
+			// continue without returning if lastmodified date could
 			// not be calculated as it might still be possible for the the
 			// directory to be a valid git repo
 			if err != nil {
 				slog.Warn(fmt.Sprintf("Unable get last modified time in %v, %v", absPath, err))
 			}
-			// skip this directory as it is most likely not a git repo
 			syncedWithRemote, syncDescription, err := getSyncStatus(absPath)
 			if err != nil {
+				// skip this directory as it is most likely not a valid git repo if git status and
+				// git for-each-ref cannot be run
 				slog.Warn(fmt.Sprintf("Unable to run git commands in %v, %v", absPath, err))
 				return
 			}
@@ -80,6 +91,7 @@ func GetReposWithDetails(root string, fetch bool) ([]Repo, error) {
 	return repos, nil
 }
 
+// returns all paths to directories in a fileSystem that contain a .git folder
 func listRepoPaths(fileSystem fs.FS) ([]string, error) {
 	var repoPaths []string
 	err := fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
@@ -114,6 +126,7 @@ func listRepoPaths(fileSystem fs.FS) ([]string, error) {
 	return repoPaths, nil
 }
 
+// run git fetch in a directory under absPath
 func gitFetch(absPath string) error {
 	cmdFetch := exec.Command("git", "fetch", "-q")
 	cmdFetch.Dir = absPath
@@ -124,14 +137,16 @@ func gitFetch(absPath string) error {
 	return nil
 }
 
+// returns the lastModified time of the most recently modified file/directory
+// in the given fileSystem while ignoring the .git folder
 func getContentLastModifiedTime(fileSystem fs.FS) (time.Time, error) {
-	// returns the lastModified time of the most recently modified file/directory
-	// in the given files system while ignoring the .git folder
 	dirInfo, err := fs.Stat(fileSystem, ".")
 	if err != nil {
 		return time.Time{}, err
 	}
 	lastModified := dirInfo.ModTime()
+	// recursively traverse through the directory and update the last modified
+	// time if any file or folder is found with an even later last modified
 	err = fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -158,11 +173,13 @@ func getContentLastModifiedTime(fileSystem fs.FS) (time.Time, error) {
 	return lastModified, nil
 }
 
+// return a slice of strings describing whether the git repo at absPath
+// has uncommitted changes, branches that are ahead/behind and untracked branches
 func getSyncStatus(absPath string) (bool, []string, error) {
-	// initialize as non-nil empty slice so that json output will be []
+	// initialize as non-nil empty slice so that json output after marshalling will be []
 	// instead of null
 	statusDescription := []string{}
-	// returns "" for repos that have all changes committed
+	// git status returns "" for repos that have all changes committed
 	cmdCommitStatus := exec.Command("git", "status", "-s")
 	cmdCommitStatus.Dir = absPath
 	out, err := cmdCommitStatus.CombinedOutput()
@@ -176,7 +193,7 @@ func getSyncStatus(absPath string) (bool, []string, error) {
 
 	// this command will return an output where each line will contain
 	// the status of the branch: "=" - synced, ">" - ahead, "<" - behind
-	// "" - no remote branch
+	// "" - no remote branch/untracked branch
 	cmdBranchStatus := exec.Command("git", "for-each-ref", "--format=%(upstream:trackshort)", "refs/heads")
 	cmdBranchStatus.Dir = absPath
 	out, err = cmdBranchStatus.CombinedOutput()
@@ -191,6 +208,7 @@ func getSyncStatus(absPath string) (bool, []string, error) {
 	return syncedWithRemote, statusDescription, nil
 }
 
+// return the author name of the last commit
 func getLastCommitAuthor(absPath string) (string, error) {
 	cmdFetch := exec.Command("git", "log", "-1", "--pretty=%an")
 	cmdFetch.Dir = absPath
@@ -220,7 +238,6 @@ func evaluateBranchSyncStatus(gitOut string) (bool, []string) {
 	// remove the trailing new line from the output because otherwise
 	// it will result in a "" in slice when the output is split on \n
 	// leading to "" being evaluated as a branch as well later
-	// remove trailing new line in description for prettier table printing
 	outWithoutEndingNewLine := strings.TrimSuffix(gitOut, "\n")
 	branches := strings.Split(outWithoutEndingNewLine, "\n")
 	for _, branch := range branches {
